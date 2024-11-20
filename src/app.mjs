f1d214d00b4d9b47ebaf280e0e5932289391966f
Yuragranovsky <yuragranovsky@gmail.com>
2024-11-20 20:23:33 +0200

    HW #69 solution

--- src/app.mjs
+++ src/app.mjs
@@ -1,15 +1,90 @@
 import MongoConnection from "./mongo/MongoConnection.mjs";
 const DB_NAME = 'sample_mflix';
 const COLLECTION_MOVIES_NAME = "movies";
+const COLLECTION_COMMENTS_NAME = "comments";
 const mongoConnection = new MongoConnection(process.env.MONGO_URI, DB_NAME);
 const collectionMovies = mongoConnection.getCollection(COLLECTION_MOVIES_NAME);
-collectionMovies.aggregate([
-    {
-      '$bucketAuto': {
-        'groupBy': '$imdb.rating', 
-        'buckets': 5
+const collectionComments = mongoConnection.getCollection(COLLECTION_COMMENTS_NAME);
+collectionComments.aggregate([
+ 
+  {
+      '$lookup': {
+          'from': 'movies',
+          'localField': 'movie_id',
+          'foreignField': '_id',
+          'as': 'movies'
       }
-    }
-  ]).toArray().then(data=>console.log("first",data));
-collectionMovies.find({}).limit(1).project({'title':1,'_id':0}).toArray()
-.then(data => console.log('second', data))
+  }, {
+      '$match': {
+          'movies.0': {
+              '$exists':true
+          }
+      }
+  }, {
+    '$limit': 5
+}, {
+      '$replaceRoot': {
+          'newRoot': {
+              '$mergeObjects': [
+                  {
+                      'title': {
+                          '$arrayElemAt': [
+                              '$movies.title', 0
+                          ]
+                      }
+                  }, '$$ROOT'
+              ]
+          }
+      }
+  }, {
+      '$project': {
+          'movies': 0,
+          'movie_id': 0
+      }
+  }
+]).toArray()
+  .then(data => console.log('query 1', data));
+collectionMovies.aggregate([
+  {
+      '$facet': {
+          'avgImdbRating': [
+              {
+                  '$group': {
+                      '_id': null, 
+                      'avgValue': {
+                          '$avg': '$imdb.rating'
+                      }
+                  }
+              }
+          ], 
+          'filteredAvgValues': [
+              {
+                  '$match': {
+                      'genres': 'Comedy', 
+                      'year': 2010
+                  }
+              }
+          ]
+      }
+  }, {
+      '$unwind': {
+          'path': '$avgImdbRating'
+      }
+  }, {
+      '$unwind': {
+          'path': '$filteredAvgValues'
+      }
+  }, {
+      '$match': {
+          '$expr': {
+              '$lt': [
+                  '$avgImdbRating.avgValue', '$filteredAvgValues.imdb.rating'
+              ]
+          }
+      }
+  }, {
+      '$project': {
+          'title': '$filteredAvgValues.title'
+      }
+  }
+]).toArray().then(data => console.log("query 2", data))
